a934ffd4e5fe1bdd9102f12676ede0aa51d0c872
Andres23Ramirez <rodrigo23ramirez@gmail.com>
2016-07-01 23:14:06 -0500

    Se genera el metodo create en le controlador de articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,12 @@
 class ArticlesController < ApplicationController
+
+#Este metodo sirve para la ruta /articles con el metodo get
+	def new
+
+	end
+
+#Este metodo sirve para la ruta /articles con el  metodo post
+	def create
+		render plain: params[:article].inspect
+	end
 end
